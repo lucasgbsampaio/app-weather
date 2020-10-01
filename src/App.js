8caed3ae973f45f01d8fe412146048fb3fdfedfe
Lucas <lukas-gabriel2002@hotmail.com>
2020-10-01 15:15:08 -0300

    estrutura inicial feita

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
 
 export default function App() {
-  return <div>Weather App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import style from './styles/Footer.module.css';
+
+export default function Footer() {
+  return <div className={style.footer}>Footer</div>;
+}

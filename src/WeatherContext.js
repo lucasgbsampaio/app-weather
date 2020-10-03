2975195d044c6b3b916fe353c781ea44341af6d7
Lucas <lukas-gabriel2002@hotmail.com>
2020-10-03 19:04:58 -0300

    context finalizado

---
+++ src/WeatherContext.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { WEATHER_GET, ONECALL_GET } from './Api/api';
+import useFetch from './Hooks/useFetch';
+
+export const WeatherContext = React.createContext();
+
+export default function WeatherStorage({ children }) {
+  const [dataCurrent, setDataCurrent] = React.useState(null);
+  const [dataOneCall, setDataOneCall] = React.useState(null);
+  const [coord, setCoord] = React.useState(null);
+  const { request } = useFetch();
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+
+  async function getCurrent(dataNow) {
+    try {
+      setError(null);
+      setLoading(true);
+      const { url, options } = WEATHER_GET(dataNow);
+      const { json } = await request(url, options);
+      setDataCurrent(json);
+      setCoord(json.coord);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function getOneCall(lat, lon) {
+    try {
+      setError(null);
+      setLoading(true);
+      const { url, options } = ONECALL_GET(lat, lon);
+      const { json } = await request(url, options);
+      setDataOneCall(json);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <WeatherContext.Provider
+      value={{
+        dataCurrent,
+        dataOneCall,
+        coord,
+        getCurrent,
+        getOneCall,
+        loading,
+        error,
+      }}
+    >
+      {children}
+    </WeatherContext.Provider>
+  );
+}

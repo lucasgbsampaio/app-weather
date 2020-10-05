719ffe9d8ca23831dc8b5de45dfed9fb5c4a7dc4
Lucas <lukas-gabriel2002@hotmail.com>
2020-10-05 19:37:31 -0300

    adicionando reatividade

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,9 +3,33 @@ import style from './styles/Header.module.css';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
+  const [city, setCity] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+  function handleChange(event) {
+    setCity(event.target.value);
+  }
+
   return (
     <header className={style.header}>
-      <nav>
+      <nav className={style.mainNav}>
+        <Link className={style.logo} to="/" aria-label="Weather App">
+          <span>The Weather App</span>
+        </Link>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="buscar"
+            value={city}
+            onChange={handleChange}
+            placeholder="Buscar cidade"
+            aria-label="Buscar cidade"
+          />
+        </form>
+      </nav>
+      <nav className={style.secondaryNav}>
         <Link to="/">Agora</Link>
         <Link to="hora/">A cada hora</Link>
         <Link to="7dias/">7 dias</Link>

8c5377c31641935f4eddc9f388247d12437fb7df
Lucas <lukas-gabriel2002@hotmail.com>
2020-10-08 17:30:18 -0300

    update

--- src/Components/Hours.js
+++ src/Components/Hours.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
 export default function Hours() {
-  return (
-    <div style={{ padding: '700px' }}>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</div>
-  );
+  return <div>Hours</div>;
 }
